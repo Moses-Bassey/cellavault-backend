cc374fd4293ac44f2c10daf36d4940043bda9662
danielokpa <danielokpa769@gmail.com>
2026-06-20 19:59:56 +0100

    Update: Added redis fee listner

--- src/modules/fees/fees.module.ts
+++ src/modules/fees/fees.module.ts
@@ -1,14 +1,16 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { RedisModule } from '../../services/redis/redis.module';
 import { PeppcruiseFees } from './entities/peppcruise-fees.entity';
 import { FeesRepository } from './repositories/fees.repository';
 import { FeesService } from './services/fees.service';
+import { FeeRedisListener } from './listeners/fees.listener';
 import { FeesController } from './controllers/fees.controller';
 
 @Module({
-  imports: [SequelizeModule.forFeature([PeppcruiseFees])],
+  imports: [SequelizeModule.forFeature([PeppcruiseFees]), RedisModule],
   controllers: [FeesController],
-  providers: [FeesService, FeesRepository],
+  providers: [FeesService, FeesRepository, FeeRedisListener],
   exports: [SequelizeModule],
 })
 export class FeesModule {}
