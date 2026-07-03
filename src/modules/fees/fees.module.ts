12761a0c17901f0a82ad925e55f0045d6198a4a7
danielokpa <danielokpa769@gmail.com>
2026-07-03 21:12:22 +0100

    Update: Added delete ride class API

--- src/modules/fees/fees.module.ts
+++ src/modules/fees/fees.module.ts
@@ -1,5 +1,6 @@
 import { Module } from '@nestjs/common';
 import { SequelizeModule } from '@nestjs/sequelize';
+import { AdminModule } from '../admins/admins.module';
 import { RedisModule } from '../../services/redis/redis.module';
 import { PeppcruiseFees } from './entities/peppcruise-fees.entity';
 import { FeesRepository } from './repositories/fees.repository';
@@ -8,7 +9,7 @@ import { FeeRedisListener } from './listeners/fees.listener';
 import { FeesController } from './controllers/fees.controller';
 
 @Module({
-  imports: [SequelizeModule.forFeature([PeppcruiseFees]), RedisModule],
+  imports: [SequelizeModule.forFeature([PeppcruiseFees]), RedisModule, AdminModule],
   controllers: [FeesController],
   providers: [FeesService, FeesRepository, FeeRedisListener],
   exports: [SequelizeModule],
