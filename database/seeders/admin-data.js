13d0a51202b512b105ece5796b1daa7da76737db
Daniel Okpa <danielokpa769@gmail.com>
2026-09-09 18:01:12 +0100

    Update: added auth APIs

--- database/seeders/admin-data.js
+++ database/seeders/admin-data.js
@@ -4,30 +4,25 @@ const argon2 = require("argon2");
 
 module.exports = {
   async up(queryInterface) {
-    const passwordHash = await argon2.hash("linus.chiagozi@gmail.com");
-    const passwordHash1 = await argon2.hash("Admin@123456!!Test");
+    const passwordHash = await argon2.hash("Admin@12345");
+    // const passwordHash1 = await argon2.hash("Admin@123456!!Test");
 
     await queryInterface.bulkInsert("admins", [
       {
         id: "7c2d3f2e-9a1b-4a9d-9f5b-3f1c2a8d9e10",
-        fullName: "Test Admin",
-        email: "danielokpa11@gmail.com",
+        name: "Super Admin",
+        email: "admin@innovations.com",
         phoneNo: "08106616246",
-        password: passwordHash1,
+        password: passwordHash,
         role: "SUPER_ADMIN",
-        imageUrl: null,
-        inviteStatus: "ACTIVE",
-        isVerified: true,
         isActive: true,
-        invitedAcceptedAt: new Date(),
-        lastLogin: null,
         createdAt: new Date(),
         updatedAt: new Date(),
         deletedAt: null,
       },
       // {
       //   id: "b4d1c9f0-2a6e-4d3a-8c11-0a9f8e2d7b33",
-      //   fullName: "Chiagozi Linus",
+      //   name: "Chiagozi Linus",
       //   email: "linus.chiagozi@gmail.com",
       //   phoneNo: "07038355876",
       //   password: passwordHash,
@@ -44,7 +39,7 @@ module.exports = {
       // },
     //   {
     //     id: "d91f0c2a-8b77-4e12-9c55-2a6d9f11c8a1",
-    //     fullName: "Pending Admin",
+    //     name: "Pending Admin",
     //     email: "pending@peppcruise.com",
     //     password: passwordHash,
     //     role: "ADMIN",
